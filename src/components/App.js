9868929fac2fd9132d6520ae3999693a95a21978
Ankur Charan <ankurcharan98@gmail.com>
2020-10-09 01:37:55 +0530

    add babelrc

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,21 @@ const App = () => {
 
     return (
         <div>
-            <button id='stateChangeButton' onClick={}>Change State</button>
+            <button id='stateChangeButton' onClick={handleStateChange}>Change State</button>
             &nbsp; &nbsp;
-            <button id='styleChangeButton' onClick={}>Change Style</button>
+            <button id='styleChangeButton' onClick={handleStyleChange}>Change Style</button>
             <br/>
-            <span id='data' className={ 
-                /** Add class 'content' to add styles based on style */
-                /** It should be toggled when Change Style is clicked */
-                /** Initially it should not be there */
-            }>
+            <span id='data' className={ /**
+             * Add class 'content' based on condition
+             * which toggles when 'Change Style' button is clicked
+             * Initially set 'content' class absent
+             */ }>
                 {
-                    /** Write 'True' or 'False' here based on conditions */
-                    /** It should be toggled when Change State button is clicked */
-                    /** Initially it should be 'False' */
+                    /**
+                     * print 'True' or 'False'
+                     * based on condition which toggles when 'Change State' button is clicked
+                     * Initially set it to 'False'
+                     */
                 }
             </span>
         </div>
